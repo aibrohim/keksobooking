4fef8ffe31b6d3d3bde068a1427d74952caadaa3
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-06-17 14:06:24 +0500

    No bugs in eslint

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-var TYPES = ["palace", "flat", "house", "bungalo"];
-var CHECKINOUT = ["12: 00", "13: 00", "14: 00"];
-var FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+'use strict';
+
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var CHECKINOUT = ['12: 00', '13: 00', '14: 00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 var getRandomNumber = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
@@ -29,47 +31,43 @@ var announcement = function () {
 
   for (var i = 0; i < 8; i++) {
     result.push({
-      "author": {
-        "avatar": "0" + (i + 1)
+      'author': {
+        'avatar': '0' + (i + 1)
       },
-      "offer": {
-        "title": "salom",
-        "address": "600, 350",
-        "price": 500,
-        "type": TYPES[Math.floor(Math.random() * 4)],
-        "rooms": Math.floor(Math.random() * 10) + 1,
-        "guests": Math.floor(Math.random() * 10) + 1,
-        "checkin": CHECKINOUT[Math.floor(Math.random() * 3)],
-        "checkout": CHECKINOUT[Math.floor(Math.random() * 3)],
-        "features": getRandomElements(FEATURES),
-        "description": "строка с описанием",
-        "photos": getRandomElements(PHOTOS)
+      'offer': {
+        'title': 'salom',
+        'address': '600, 350',
+        'price': 500,
+        'type': TYPES[Math.floor(Math.random() * 4)],
+        'rooms': Math.floor(Math.random() * 10) + 1,
+        'guests': Math.floor(Math.random() * 10) + 1,
+        'checkin': CHECKINOUT[Math.floor(Math.random() * 3)],
+        'checkout': CHECKINOUT[Math.floor(Math.random() * 3)],
+        'features': getRandomElements(FEATURES),
+        'description': 'строка с описанием',
+        'photos': getRandomElements(PHOTOS)
       },
-      "location": {
-        "x": Math.floor(Math.random() * 1200) + 1,
-        "y": Math.floor(Math.random() * (630 - 130) ) + 130
+      'location': {
+        'x': Math.floor(Math.random() * 1200) + 1,
+        'y': Math.floor(Math.random() * (630 - 130)) + 130
       }
-    })
+    });
   }
 
   return result;
-}
+};
 
-console.log(announcement());
+var map = document.querySelector('.map');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-var map = document.querySelector(".map");
-var pinTemplate = document.querySelector("#pin").content.querySelector(".map__pin");
+map.classList.remove('map--faded');
 
-map.classList.remove("map--faded");
-
-announcement().forEach(function(element) {
+announcement().forEach(function (element) {
   var pin = pinTemplate.cloneNode(true);
-  pin.setAttribute("style", "left: " + element.location.x + "px; top: " + element.location.y + "px;");
-  document.querySelector(".map__pins").appendChild(pin);
+  pin.setAttribute('style', 'left: ' + element.location.x + 'px; top: ' + element.location.y + 'px;');
+  document.querySelector('.map__pins').appendChild(pin);
 
-  var pinImg = pin.querySelector("img");
-  pinImg.setAttribute("src", "img/avatars/user" + element.author.avatar + ".png");
-  pinImg.setAttribute("alt", element.offer.title);
-
-  console.log(element.location.x);
-})
+  var pinImg = pin.querySelector('img');
+  pinImg.setAttribute('src', 'img/avatars/user' + element.author.avatar + '.png');
+  pinImg.setAttribute('alt', element.offer.title);
+});
