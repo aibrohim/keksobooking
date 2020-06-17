862f88df1dc54c46117ed573f125105cfe0f9823
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-06-17 21:02:24 +0500

    Changes

--- js/main.js
+++ js/main.js
@@ -105,9 +105,8 @@ announcement().forEach(function (element) {
   popup.querySelector('.popup__type').textContent = dictionary[element.offer.type];
   popup.querySelector('.popup__text--capacity').textContent = element.offer.rooms + ' комнаты для ' + element.offer.guests + ' гостей';
   popup.querySelector('.popup__text--time').textContent = 'Заезд после ' + element.offer.checkin + ', выезд до ' + element.offer.checkout;
-  console.log(element.offer.features);
 
-  popup.querySelector('.popup__features').appendChild(showFeatures(element.offer.features));
+  popup.querySelector('.popup__features').innerHTML = showFeatures(element.offer.features);
 
 
   popup.querySelector('.popup__description').textContent = element.offer.description;
