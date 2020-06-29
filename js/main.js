e086914627ae00d2fdc6ca2926c5ccd0fd905798
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-06-29 17:34:17 +0500

    Task 2 from module4 almost ready

--- js/main.js
+++ js/main.js
@@ -5,11 +5,22 @@ var CHECKINOUT = ['12: 00', '13: 00', '14: 00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var fragment = document.createDocumentFragment();
-var map = document.querySelector('.map');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var cardTemplate = document.querySelector('#card').content.querySelector('article');
 var MAP__PINS = document.querySelector('.map__pins');
 var MAP = document.querySelector('.map');
+var MAIN_PIN = document.querySelector('.map__pin--main');
+var AD_FORM = document.querySelector('.ad-form');
+var FILTER_FORM = document.querySelectorAll('.map__filters > *');
+var FIELDSETS = document.querySelectorAll('.ad-form fieldset');
+var MAP_OVERLAY = MAP.querySelector('.map__overlay');
+var locationInput = document.querySelector('#address');
+var roomNumber = document.querySelector('#room_number');
+var capacity = document.querySelector('#capacity');
+var MAP_WIDTH = MAP_OVERLAY.offsetWidth;
+var MAP_HEIGHT = MAP_OVERLAY.offsetHeight;
+var MAIN_PIN_WIDTH = MAIN_PIN.offsetWidth;
+var MAIN_PIN_HEIGHT = MAIN_PIN.offsetHeight + 20;
 
 var dictionary = {
   palace: 'Дворец',
@@ -70,7 +81,73 @@ var announcements = function () {
   return result;
 };
 
-map.classList.remove('map--faded');
+var startProgram = function () {
+  MAP.classList.remove('map--faded');
+  MAP__PINS.appendChild(fragment);
+  AD_FORM.classList.remove('ad-form--disabled');
+  locationInput.value = (570 + (MAIN_PIN_WIDTH / 2)) + ', ' + (375 + (MAIN_PIN_HEIGHT));
+
+  FIELDSETS.forEach(function (fieldset) {
+    fieldset.disabled = false;
+  });
+
+  FILTER_FORM.forEach(function (item) {
+    item.disabled = false;
+  });
+};
+
+var addOption = function (optionValue) {
+  var option = document.createElement('option');
+  option.value = optionValue;
+  option.textContent = 'для ' + optionValue + ' гостей';
+  if (optionValue === 0) {
+    option.textContent = 'не для гостей';
+  }
+  capacity.appendChild(option);
+};
+
+locationInput.value = MAP_WIDTH / 2 + ', ' + MAP_HEIGHT / 2;
+
+FIELDSETS.forEach(function (fieldset) {
+  fieldset.disabled = true;
+});
+
+FILTER_FORM.forEach(function (item) {
+  item.disabled = true;
+});
+
+MAIN_PIN.addEventListener('mousedown', function (evt) {
+  if (evt.button === 0) {
+    startProgram();
+  }
+});
+
+MAIN_PIN.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    startProgram();
+  }
+});
+
+MAIN_PIN.addEventListener('mousemove', function () {});
+
+roomNumber.addEventListener('change', function () {
+  if (roomNumber.value === '1') {
+    capacity.innerHTML = '';
+    addOption(1);
+  } else if (roomNumber.value === '2') {
+    capacity.innerHTML = '';
+    addOption(1);
+    addOption(2);
+  } else if (roomNumber.value === '3') {
+    capacity.innerHTML = '';
+    addOption(1);
+    addOption(2);
+    addOption(3);
+  } else if (roomNumber.value === '100') {
+    capacity.innerHTML = '';
+    addOption(0);
+  }
+});
 
 announcements().forEach(function (element) {
   var pin = pinTemplate.cloneNode(true);
@@ -82,8 +159,6 @@ announcements().forEach(function (element) {
 
   fragment.appendChild(pin);
 });
-
-MAP__PINS.appendChild(fragment);
 
 var showPopup = function (objectItem) {
   var popup = cardTemplate.cloneNode(true);
@@ -126,4 +201,6 @@ var showPopup = function (objectItem) {
   return MAP.insertBefore(popup, document.querySelector('.map__filters-container'));
 };
 
-showPopup(announcements()[0]);
+if (MAP_WIDTH === 3000) {
+  showPopup(announcements()[0]);
+}
