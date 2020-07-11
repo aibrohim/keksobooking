50baa6377de771535f9a93c58562b1018ff220b0
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-07-11 16:49:25 +0500

    mistakes were corrected

--- js/main-pin.js
+++ js/main-pin.js
@@ -2,6 +2,7 @@
 
 window.mainPin = (function () {
   var mainPin = document.querySelector('.map__pin--main');
+  var addressInput = document.querySelector('#address');
 
   mainPin.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
@@ -11,6 +12,8 @@ window.mainPin = (function () {
       y: evt.clientY
     };
 
+    addressInput.value = (mainPin.style.left.replace('px', '')) + ', ' + (mainPin.style.top.replace('px', ''));
+
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
 
@@ -19,32 +22,27 @@ window.mainPin = (function () {
         y: startCoords.y - moveEvt.clientY
       };
 
-      // if (mainPin.offsetLeft <= -32) {
-      //   shift = {
-      //     x: 0,
-      //     y: startCoords.y - moveEvt.clientY
-      //   };
-      // }
-
       startCoords = {
         x: moveEvt.clientX,
         y: moveEvt.clientY,
       };
 
+
       mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
       mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+
+      addressInput.value = (mainPin.offsetLeft - shift.x) + ', ' + (mainPin.offsetTop - shift.y);
 
       if (mainPin.offsetLeft <= -32) {
         mainPin.style.left = (mainPin.offsetLeft + shift.x) + 'px';
       } else if (mainPin.offsetLeft >= 1168) {
         mainPin.style.left = (mainPin.offsetLeft + shift.x) + 'px';
-      } else if (mainPin.offsetTop <= 130) {
+      }
+
+      if (mainPin.offsetTop <= 130) {
         mainPin.style.top = (mainPin.offsetTop + shift.y) + 'px';
       } else if (mainPin.offsetTop >= 630) {
         mainPin.style.top = (mainPin.offsetTop + shift.y) + 'px';
-      } else if (mainPin.offsetTop >= 630 && mainPin.offsetLeft <= -32) {
-        mainPin.style.top = (mainPin.offsetTop + shift.y) + 'px';
-        mainPin.style.left = (mainPin.offsetLeft + shift.x) + 'px';
       }
     };
 
