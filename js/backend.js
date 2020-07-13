8819d2fe59fda5c3fde8207077ac537b5a722d6d
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-07-13 19:24:25 +0500

    All pins have shown :D

--- js/backend.js
+++ js/backend.js
@@ -1,57 +1,20 @@
 'use strict';
 
 window.backend = (function () {
-  var onError = function (message) {
-    console.error(message);
-  };
+  var URL = 'https://javascript.pages.academy/keksobooking/data';
 
-  var onSuccess = function (data) {
-    var pins = data;
+  return {
+    data: function (onSuccess) {
+      var xhr = new XMLHttpRequest();
 
-    console.log(pins);
-  };
+      xhr.responseType = 'json';
 
-  var xhr = new XMLHttpRequest();
-
-  xhr.responseType = 'json';
-
-  xhr.addEventListener('load', function () {
-    var error;
-
-    switch (xhr.status) {
-      case 200:
+      xhr.addEventListener('load', function () {
         onSuccess(xhr.response);
-        break;
+      });
 
-      case 400:
-        error = 'Неверный запрос';
-        break;
-      case 401:
-        error = 'Пользователь не авторизован';
-        break;
-      case 404:
-        error = 'Ничего не найдено';
-        break;
-
-      default:
-        error = 'Cтатус ответа: : ' + xhr.status + ' ' + xhr.statusText;
-    }
-
-    if (error) {
-      onError(error);
-    }
-  });
-
-  xhr.addEventListener('error', function () {
-    onError('Произошла ошибка соединения');
-  });
-
-  xhr.addEventListener('timeout', function () {
-    onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-  });
-
-  xhr.timeout = 10000;
-
-  xhr.open('GET', 'https://javascript.pages.academy/keksobooking/data');
-  xhr.send();
+      xhr.open('GET', URL);
+      xhr.send();
+    },
+  };
 })();
--- js/pin.js
+++ js/pin.js
@@ -4,17 +4,21 @@ window.pins = (function () {
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var fragment = document.createDocumentFragment();
 
-  window.data.announcements().forEach(function (element) {
-    var pin = pinTemplate.cloneNode(true);
-    pin.setAttribute('style', 'left: ' + element.location.x + 'px; top: ' + element.location.y + 'px;');
+  var showPins = function (data) {
+    data.forEach(function (element) {
+      var pin = pinTemplate.cloneNode(true);
+      pin.setAttribute('style', 'left: ' + element.location.x + 'px; top: ' + element.location.y + 'px;');
 
-    var pinImg = pin.querySelector('img');
-    pinImg.setAttribute('src', 'img/avatars/user' + element.author.avatar + '.png');
-    pinImg.setAttribute('alt', element.offer.title);
+      var pinImg = pin.querySelector('img');
+      pinImg.setAttribute('src', element.author.avatar);
+      pinImg.setAttribute('alt', element.offer.title);
 
-    window.mapPins = document.querySelectorAll('.map__pin');
-    fragment.appendChild(pin);
-  });
+      window.mapPins = document.querySelectorAll('.map__pin');
+      fragment.appendChild(pin);
+    });
+  };
+
+  window.backend.data(showPins);
 
   return {
     fragment: fragment
