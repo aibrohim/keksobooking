4ef981d9208e58c0ea61829a7682b9b73075c545
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-07-23 23:55:02 +0500

    modified

--- js/pins.js
+++ js/pins.js
@@ -3,13 +3,13 @@
 window.pins = (function () {
   var MAX_PINS = 5;
   var MAP_PINS = document.querySelector('.map__pins');
-  var dataTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+  var adTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var fragment = document.createDocumentFragment();
 
   var showPins = function (data) {
     for (var i = 0; i < data.length; i++) {
       if (i < MAX_PINS) {
-        var pin = dataTemplate.cloneNode(true);
+        var pin = adTemplate.cloneNode(true);
         pin.setAttribute('style', 'left: ' + data[i].location.x + 'px; top: ' + data[i].location.y + 'px;');
 
         var pinImg = pin.querySelector('img');
